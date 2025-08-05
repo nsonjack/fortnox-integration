1e6b65c69a25ef5e844df705438783fc9fc02ebe
nsonjack <info@nsonbolagen.se>
2025-08-05 11:37:14 +0200

    Update authorize.php

--- authorize.php
+++ authorize.php
@@ -1,4 +1,3 @@
 <?php
-header("Location: https://apps.fortnox.se/oauth-v1/authorize?client_id=4kE57REPVc85&redirect_uri=https://nsonbolagen.se/fortnox-callback.php&scope=customer&response_type=code");
-exit;
+echo "Authorize.php laddades!";
 ?>
